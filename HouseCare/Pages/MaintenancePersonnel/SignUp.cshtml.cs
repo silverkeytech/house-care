4ca5c5cb238c90ea9edc9cf492e506dd7622e1fc
Salma Ahmed Mahfouz <93517375+salma-ahmed9@users.noreply.github.com>
2023-08-30 19:11:55 +0300

    Homepage finished

---
+++ HouseCare/Pages/MaintenancePersonnel/SignUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HouseCare.Pages.MaintenancePersonnel
+{
+    public class SignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
