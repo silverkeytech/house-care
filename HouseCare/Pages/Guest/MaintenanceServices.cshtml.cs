f3d8c5246cd426e481a45437b0177227bd96a281
Salma Ahmed Mahfouz <93517375+salma-ahmed9@users.noreply.github.com>
2023-08-31 14:41:32 +0300

    SignUp added

---
+++ HouseCare/Pages/Guest/MaintenanceServices.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HouseCare.Pages.Guest
+{
+    public class MaintenanceServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
