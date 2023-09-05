7497550d758fd802abbb70f1b764a1681c928be2
nourhanhelmy1 <nourhanhelmy01@outlook.com>
2023-09-05 23:59:47 +0300

    Added maintenance requests model

---
+++ HouseCare/Models/MaintenanceRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseCare.Models
+{
+    public class MaintenanceRequest
+    {
+        public string RequesterName { get; set; }
+        public string City { get; set; }
+        public DateTime AssignedDate { get; set; }
+        public string RequestCategory { get; set; }
+    }
+}
