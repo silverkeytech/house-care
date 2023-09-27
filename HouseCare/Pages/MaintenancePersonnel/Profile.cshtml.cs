6313c69b6419da68b46e5d31e0c035d83ccd1c7c
Salma Ahmed Mahfouz <93517375+salma-ahmed9@users.noreply.github.com>
2023-09-28 01:20:05 +0300

    profile page finished

--- HouseCare/Pages/MaintenancePersonnel/Profile.cshtml.cs
+++ HouseCare/Pages/MaintenancePersonnel/Profile.cshtml.cs
@@ -1,7 +1,11 @@
 using EdgeDB;
+using HouseCare.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HouseCare.Pages.MaintenancePersonnel
 {
@@ -10,7 +14,6 @@ namespace HouseCare.Pages.MaintenancePersonnel
     {
         private readonly EdgeDBClient _edgeclient;
         public Models.MaintenancePersonnel Personnel { get; set; }
-
         public ProfileModel(EdgeDBClient client)
         {
             _edgeclient = client;
@@ -20,20 +23,7 @@ namespace HouseCare.Pages.MaintenancePersonnel
         {
             string email = User.FindFirst(ClaimTypes.Email)?.Value;
 
-            var personnelCollection = await _edgeclient.QueryAsync<Models.MaintenancePersonnel>(
-                @"
-                SELECT MaintenancePersonnel {
-                    FirstName := .first_name,
-                    LastName := .last_name,
-                    Email := .email,
-                    Phone := .phone,
-                    Image := .image,
-                    Id := .id
-                }
-                FILTER .email = <str>$email", new { email });
-
-            Personnel = personnelCollection.FirstOrDefault();
-
+            Personnel = await _edgeclient.QuerySingleAsync<Models.MaintenancePersonnel>("SELECT MaintenancePersonnel {FirstName := .first_name , LastName := .last_name , Image := .image , Phone := .phone , Email := .email} FILTER .email = <str>$email", new Dictionary<string, object?> { { "email", email } });
             if (Personnel == null)
             {
                 return NotFound();
@@ -44,32 +34,54 @@ namespace HouseCare.Pages.MaintenancePersonnel
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            if (string.IsNullOrEmpty(Personnel.Phone) || string.IsNullOrEmpty(Personnel.Email) || string.IsNullOrEmpty(Personnel.FirstName) || string.IsNullOrEmpty(Personnel.LastName))
             {
+                ModelState.AddModelError("ContactError", "All fields must be filled");
                 return Page();
             }
-
             string email = User.FindFirst(ClaimTypes.Email)?.Value;
-
-            var updateQuery = @"
-            UPDATE MaintenancePersonnel
-                FILTER .email = <str>$email
-                SET {first_name := <str>$FirstName,
-                last_name := <str>$LastName,
-                phone := <str>$PhoneNumber,
-                email := <str>$email
-            }";
-
-            await _edgeclient.ExecuteAsync(updateQuery, new Dictionary<string, object?>
+            if (Personnel.ImageFile != null)
             {
-               {"FirstName", Request.Form["FirstName"]},
-               {"LastName", Request.Form["LastName"]},
-               {"PhoneNumber", Request.Form["PhoneNumber"]},
-               {"email", email}
-            });
-            
+                var filePath = Path.Combine("wwwroot/Assets/Images/Personnel", Personnel.ImageFile.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Personnel.ImageFile.CopyTo(fileStream);
+                }
+                Personnel.Image = Personnel.ImageFile.FileName;
+                var query = "Update MaintenancePersonnel FILTER .email = <str>$email SET {first_name := <str>$FirstName,  last_name := <str>$LastName, phone := <str>$PhoneNumber, email := <str>$Email ,  image := <str>$image } ";
+                await _edgeclient.ExecuteAsync(query, new Dictionary<string, object?>
+                {
+                   {"email",email},
+                   {"FirstName", Personnel.FirstName},
+                   {"LastName", Personnel.LastName},
+                   {"PhoneNumber", Personnel.Phone},
+                   {"Email", Personnel.Email},
+                   {"image", Personnel.Image},
+
+
+                });
+            }
+            else
+            {
+                var query = "Update MaintenancePersonnel FILTER .email = <str>$email SET {first_name := <str>$FirstName,  last_name := <str>$LastName, phone := <str>$PhoneNumber, email := <str>$Email } ";
+                await _edgeclient.ExecuteAsync(query, new Dictionary<string, object?>
+                {
+                   {"email",email},
+                   {"FirstName", Personnel.FirstName},
+                   {"LastName", Personnel.LastName},
+                   {"PhoneNumber", Personnel.Phone},
+                   {"Email", Personnel.Email},
+
+                });
+
+            }
+            if (email != Personnel.Email)
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                return RedirectToPage("Login");
+            }
             // Redirect back to the profile page after updating
-            return RedirectToPage("/Profile");
+            return RedirectToPage("Profile");
         }
 
     }
